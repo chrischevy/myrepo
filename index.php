698c77d1449a36099026ef3a3a0bdb385036fd4d
EC2 Default User <ec2-user@ip-172-31-31-67.eu-west-3.compute.internal>
2018-11-14 15:04:56 +0000

    new index.php

--- index.php
+++ index.php
@@ -1,18 +1,15 @@
 <?php
-$VALEUR_hote='mariadb.chv5difr8xme.eu-west-3.rds.amazonaws.com';
+$VALEUR_hote='paris2018.cmnvzvombnsh.eu-west-3.rds.amazonaws.com';
 $VALEUR_port='3306';
-$VALEUR_nom_bd='MariaDB';
-$VALEUR_user='adminDB';
-$VALEUR_mot_de_passe='adminDB!';
+$VALEUR_nom_bd='paris2018';
+$VALEUR_user='paris2018';
+$VALEUR_mot_de_passe='paris2018';
 $connexion = new PDO('mysql:host='.$VALEUR_hote.';port='.$VALEUR_port.';dbname='.$VALEUR_nom_bd, $VALEUR_user, $VALEUR_mot_de_passe);
-
-$resultats=$connexion->query("SELECT * FROM students ");
+$resultats=$connexion->query("SELECT * FROM student ");
 $resultats->setFetchMode(PDO::FETCH_OBJ);
 while( $resultat = $resultats->fetch() )
 {
         echo 'stagiaire : '.$resultat->nom.'<br>';
 }
 $resultats->closeCursor();
-
 ?>
-
